ce319811020b48090a959f208c3a3fa91c71d32a
Candidat-005 <g.thomas@laposte.net>
2024-02-13 11:43:28 +0100

    Saison 3 exo 2, 3 et 7

--- s3/exercice5.js
+++ s3/exercice5.js
@@ -1,1 +1,5 @@
 // Exercice 5: Créer une fonction de temporisation
+
+setTimeout(() => {
+    console.log("1 sec retard");
+}, 1000);
--- s3/exercice7.js
+++ s3/exercice7.js
@@ -1,1 +1,5 @@
 // Exercice 7: Implémenter une fonction de réduction personnalisée
+
+var array = [1, 2, 3, 4, 5];
+var test = array.reduce((a, b) => a / b);
+console.log(test);
