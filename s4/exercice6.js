6db16cb1c34b6cbf8cf39f8fe9c6f30da9165205
Candidat-005 <g.thomas@laposte.net>
2024-02-14 09:54:48 +0100

    S4/exo6 fin

--- s4/exercice6.js
+++ s4/exercice6.js
@@ -1,8 +1,16 @@
 // Exercice 6: Utilisation de fetch pour récupérer des données d'une API
 
-fetch()
-.then(function(){
+fetch('https://regres.in/api/users',{
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
 
-});
-
-
+    body: JSON.stringify({
+        name: 'User1'
+    })
+}).then(res =>  {
+    return res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
